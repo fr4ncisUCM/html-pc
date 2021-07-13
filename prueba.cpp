a91e3c62bd61406883663bb4bedfc6beee6ddc77
Fran calvo <francisco.calvo.gonz@gmail.com>
2021-07-13 19:55:01 +0200

    cpp2

---
+++ prueba.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include"descuentos.h"
+#include"clientes.h"
+#include "main.h"
+
+using namespace std;
+
+int main() {
+
+	mainf();
+
+	return 0;
+}
+
+void mainf()
+{
+	tDescuentos tabla;
+	string nombrearchivo = "descuentos.txt";
+	cargarClientes(nombrearchivo, tabla);
+	ifstream archivo;
+
+	archivo.open("clientes.txt", ios::in);
+	if (archivo.fail()) {
+		cout << "f";
+	}
+	tListaClientes listac = cargarClientes(archivo);
+
+	registrarImportes(listac, tabla);
+
+	ordenarClientes(listac);
+
+	imprimirClientes(listac, listac.conntclient);
+
+	seleccionarPremiados(listac, 3, "premiados.txt");
+}
+
+//float tListaCompra[8]
+//
